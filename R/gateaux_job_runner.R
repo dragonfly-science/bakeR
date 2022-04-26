ccf36476c80eeca1b32d96c8dd97028bd8b86872
Philipp-Neubauer <neubauer.phil@gmail.com>
2022-04-26 16:38:07 +1200

    update docs

--- R/gateaux_job_runner.R
+++ R/gateaux_job_runner.R
@@ -23,7 +23,6 @@
 #'                    JWT = JWT)
 #' @importFrom magrittr %>%
 #' @export
-magrittr::`%>%`
 
 gateaux_job_runner <- function(pars_list = NULL,
                                report_name,
@@ -43,19 +42,22 @@ gateaux_job_runner <- function(pars_list = NULL,
 
     tag = sprintf('"%s":"%s"',"TAG",names(pars_list[l]))
     if(length(pars_list[[l]]$pars)>0){
-      envs = lapply(1:length(pars_list[[l]]$pars), function(ll) sprintf('"%s":"%s"',names(pars_list[[l]]$pars)[ll],pars_list[[l]]$pars[ll]))
+      envs = lapply(1:length(pars_list[[l]]$pars),
+                    function(ll) sprintf('"%s":"%s"',names(pars_list[[l]]$pars)[ll],pars_list[[l]]$pars[ll]))
       envs = paste(unlist(envs), collapse = ',')
       envs = paste(tag, envs, sep=',')
     } else {envs = tag}
 
     if(length(pars_list[[l]]$wants)>0){
-      wants = lapply(1:length(pars_list[[l]]$wants), function(ll) sprintf('"%s:%s"',names(pars_list[[l]]$wants)[ll],as.character(pars_list[[l]]$wants[ll][[1]])))
+      wants = lapply(1:length(pars_list[[l]]$wants),
+                     function(ll) sprintf('"%s:%s"',names(pars_list[[l]]$wants)[ll],as.character(pars_list[[l]]$wants[ll][[1]])))
       wants = paste(unlist(wants), collapse = ',')
       wants = paste0(', "wants":[',wants,']')
     } else {wants = ''}
 
     if(length(pars_list[[l]]$requires)>0){
-      requires = lapply(1:length(pars_list[[l]]$requires), function(ll) sprintf('"%s:%s"',names(pars_list[[l]]$requires)[ll],as.character(pars_list[[l]]$requires[ll][[1]])))
+      requires = lapply(1:length(pars_list[[l]]$requires),
+                        function(ll) sprintf('"%s:%s"',names(pars_list[[l]]$requires)[ll],as.character(pars_list[[l]]$requires[ll][[1]])))
       requires = paste(unlist(requires), collapse = ',')
       requires = paste0(', "requires":[',requires,']')
     } else {requires = ''}
@@ -79,14 +81,17 @@ gateaux_job_runner <- function(pars_list = NULL,
       rr <- dplyr::bind_cols(rr %>% select(-parameters),rr$parameters)    # added 'dplyr::' to solve a error message: could not find function "bind_cols"
       rr <- dplyr::bind_cols(rr %>% select(-env),rr$env)                  # added 'dplyr::' to solve a error message: could not find function "bind_cols"
       readr::write_csv(rr%>% select(-variant),path = paste0(prefix,'-joblist.csv'),append = append)
-    } else {jsonlite::fromJSON(ret)}
+    } else {
+      jsonlite::fromJSON(ret)
+      }
   }
   )
 
 }
 
 
-#' Modify par list to run all jubs listed with a certain env variable - useful to temporarily try or alter a parameter setup without modifying the original setup
+#' Modify par list to run all jobs listed with a certain env variable -
+#' useful to temporarily try or alter a parameter setup without modifying the original setup
 #'
 #' @param pars_list A named list of named lists of environment variables and job requirements - one list per job.
 #' Each named list defines a job tag (name), with up to three sub-lists:
@@ -96,7 +101,7 @@ gateaux_job_runner <- function(pars_list = NULL,
 #' @param var env variable to create or alter.
 #' @param value env variable value to assign. Can be a single value or a vector or the same length as pars_list.
 #' @author Dragonfly bakery
-#' @return JSON API return
+#' @return Altered parameter list to run with gateaux_job_runner
 #' @export
 
 run_all_with <- function(pars_list, var, value, set="pars",rm_vals=F){
